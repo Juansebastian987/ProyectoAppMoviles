8645863882f6e06a2d92beb139435cedb81106de
Juansebastian987 <juanse.bastian2012@hotmail.com>
2020-05-01 19:42:06 -0500

    files

---
+++ app/src/main/java/com/application/proyectoappmoviles/view/login/Login.java
@@ -0,0 +1,16 @@
+package com.application.proyectoappmoviles.view.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.application.proyectoappmoviles.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

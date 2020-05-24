41952b4c93ff8a446a3925b4e122ab1af02f1932
Bryan Garcés <bragapo23@gmail.com>
2020-05-24 18:07:41 -0500

    Commit for fixes 2

---
+++ app/src/main/java/com/application/proyectoappmoviles/view/home/HomeCActivity.java
@@ -0,0 +1,10 @@
+package com.application.proyectoappmoviles.view.home;
+
+import android.widget.Button;
+
+public class HomeCActivity {
+
+    private Button btn_help;
+    private Button btn_product;
+    private Button btn_service;
+}
